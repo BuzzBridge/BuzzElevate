944116d32ac4c578b0c7a06ba432341749fe50d3
Asees <asees1999@gmail.com>
2025-01-03 20:58:36 +0530

    added footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { FaInstagram } from "react-icons/fa"; // Import Instagram icon
 import "../styles/Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2025 Elevate Academy. All rights reserved.</p>
+      <div className="footer-text">
+        <p>© 2025 Elevate Academy. All rights reserved.</p>
+        <p className="managed-by">Powered By Buzz Bridge Inc.</p>
+      </div>
       <div className="social-links">
-        <a href="#">Instagram</a>
-        <p className="managed-by">Managed By Buzz Bridge Inc.</p>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <FaInstagram size={40} /> {/* Increased icon size */}
+        </a>
       </div>
     </footer>
   );
