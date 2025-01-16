fe84f83fe2ca373371932e933d4796eb54d92340
Asees <asees1999@gmail.com>
2025-01-16 11:47:07 +0530

    added hero css

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
        <Header />
-      {/* <Hero registrationRef={registrationRef} /> */}
+      <Hero registrationRef={registrationRef} />
       {/* <Courses />  */}
       <About />
       <div ref={registrationRef}> 
